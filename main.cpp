1757169b64db9af2d82087dc0d32efcd18e37e89
Guillermo Mora <guillermijas@gmail.com>
2018-04-03 17:16:53 +0200

    Replace tsv for csv

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 #include "util.h"
 
 using namespace std;
-const char* TSV_PATH = "incidents.tsv";
-const char* TSV_RESULT_PATH = "incidents_found.tsv";
+const char* CSV_PATH = "incidents.csv";
+const char* CSV_RESULT_PATH = "incidents_found.csv";
 
-void simple_search(const string &column, const string &value, const string &tsv_path = TSV_PATH);
+void simple_search(const string &column, const string &value, const string &csv_path = CSV_PATH);
 int column_position(const string &column);
-vector<string> tsv_header();
-void search_value(const string &value, int position, const string &tsv_path);
+vector<string> csv_header();
+void search_value(const string &value, int position, const string &csv_path);
 void intersection_search(const vector<string> &params);
 void union_search(const vector<string> &params);
 void search_multiple_values(const string &first_value, int first_pos, const string &second_value, int second_pos);
@@ -33,22 +33,22 @@ int main(int argc, char* argv[]){
   auto end = chrono::system_clock::now();
   chrono::duration<double> elapsed_seconds = end - start;
   cout << "Finished in " << elapsed_seconds.count() << " s." << endl;
-  cout << "You can see the results in 'incidents_found.tsv'" << endl;
+  cout << "You can see the results in 'incidents_found.csv'" << endl;
 
   return 0;
 }
 
 
-void simple_search(const string &column, const string &value, const string &tsv_path){
+void simple_search(const string &column, const string &value, const string &csv_path){
   int column_pos = column_position(column);
   if (column_pos > -1){
-    search_value(value, column_pos, tsv_path);
+    search_value(value, column_pos, csv_path);
   }else
     cout << "Column " << column << " not found." << endl;
 }
 
 int column_position(const string &column) {
-  vector<string> headers = tsv_header();
+  vector<string> headers = csv_header();
   int i = 0;
   bool found = false;
 
@@ -61,41 +61,41 @@ int column_position(const string &column) {
   return i;
 }
 
-vector<string> tsv_header() {
+vector<string> csv_header() {
   vector<string> columns;
   string row, value, token;
-  ifstream file(TSV_PATH);
+  ifstream file(CSV_PATH);
 
   getline(file, row);
   istringstream first_row(row);
 
-  while(getline(first_row, token, '\t'))
+  while(getline(first_row, token, ';'))
     columns.push_back(token);
 
   return columns;
 }
 
 
-void search_value(const string &value, int position, const string &tsv_path) {
-  string tsv_row, row_elem;
+void search_value(const string &value, int position, const string &csv_path) {
+  string csv_row, row_elem;
   vector<string> vect_row;
   vector<string> matching_rows;
-  ifstream file(tsv_path);
+  ifstream file(csv_path);
   int rows_ctr = 0;
 
   // Insert header first
-  getline(file, tsv_row);
-  matching_rows.push_back(tsv_row);
+  getline(file, csv_row);
+  matching_rows.push_back(csv_row);
 
   // Insert matching data
-  while(getline(file, tsv_row)) {
+  while(getline(file, csv_row)) {
     vect_row.clear();
-    stringstream buffer(tsv_row);
-    while(getline(buffer, row_elem, '\t'))
+    stringstream buffer(csv_row);
+    while(getline(buffer, row_elem, ';'))
       vect_row.push_back(util::str_tolower(row_elem));
 
     if(util::str_contains_lower(vect_row[position], value))
-      matching_rows.push_back(tsv_row);
+      matching_rows.push_back(csv_row);
 
     rows_ctr++;
   }
@@ -110,7 +110,7 @@ void intersection_search(const vector<string> &params) {
   cout << "First condition: " << params[0] << " " << params[1] << endl;
   simple_search(params[0], params[1]);
   cout << "Second condition: " << params[3] << " " << params[4] << endl;
-  simple_search(params[3], params[4], TSV_RESULT_PATH);
+  simple_search(params[3], params[4], CSV_RESULT_PATH);
 }
 
 void union_search(const vector<string> &params) {
@@ -123,25 +123,25 @@ void union_search(const vector<string> &params) {
 }
 
 void search_multiple_values(const string &first_value, int first_pos, const string &second_value, int second_pos) {
-  string tsv_row, row_elem;
+  string csv_row, row_elem;
   vector<string> vect_row;
   vector<string> matching_rows;
-  ifstream file(TSV_PATH);
+  ifstream file(CSV_PATH);
   int rows_ctr = 0;
 
   // Insert header first
-  getline(file, tsv_row);
-  matching_rows.push_back(tsv_row);
+  getline(file, csv_row);
+  matching_rows.push_back(csv_row);
 
   // Insert matching data
-  while(getline(file, tsv_row)) {
+  while(getline(file, csv_row)) {
     vect_row.clear();
-    stringstream buffer(tsv_row);
-    while(getline(buffer, row_elem, '\t'))
+    stringstream buffer(csv_row);
+    while(getline(buffer, row_elem, ';'))
       vect_row.push_back(util::str_tolower(row_elem));
 
     if(util::str_contains_lower(vect_row[first_pos], first_value) || util::str_contains_lower(vect_row[second_pos], second_value))
-      matching_rows.push_back(tsv_row);
+      matching_rows.push_back(csv_row);
 
     rows_ctr++;
   }
